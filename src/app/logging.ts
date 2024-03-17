bc88959b7deb3e9dca90211d2cc1e0d7a0ad7920
nnivxix <adhe.hanasa@gmail.com>
2024-03-17 12:37:55 +0700

    fix: logging file

--- src/app/logging.ts
+++ src/app/logging.ts
@@ -1,9 +1,10 @@
 import winston from "winston";
 
 export const logger = winston.createLogger({
-  level: 'debug',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.Console()
-  ]
-})
+	level: "debug",
+	format: winston.format.json(),
+	transports: [
+		new winston.transports.File({ filename: "logs/debug.log", level: "debug" }),
+		new winston.transports.File({ filename: "logs/express.log" }),
+	],
+});
